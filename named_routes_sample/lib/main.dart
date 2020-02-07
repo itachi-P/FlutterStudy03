e8a402d20bd14e0bf7825b582228cf6cdfb29fe6
itachi-P <itachip38@gmail.com>
2020-02-08 08:53:35 +0900

    Problem occurs in Named Routes '/'.

---
+++ named_routes_sample/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo 05',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: FirstPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => FirstPage(),
+        '/second': (context) => SecondPage(),
+      },
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('First Page')),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pushNamed(
+              context,
+              '/second',
+              arguments: 'messeageFromFirstなんですの。',
+            );
+          },
+          child: Text('Next Page'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var messageFromFirst = ModalRoute.of(context).settings.arguments;
+    print(messageFromFirst);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Page')),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go Back'),
+        ),
+      ),
+    );
+  }
+}
