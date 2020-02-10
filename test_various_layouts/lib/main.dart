0e79bc93c6556f11b1cb633d2201288110c03263
itachi-P <itachip38@gmail.com>
2020-02-10 11:35:43 +0900

    Create new app for learning flutter provides various rayouts.

---
+++ test_various_layouts/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  debugPaintSizeEnabled = true;
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alignment',
+      home: Center(
+        child:Container(
+          color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container( color: Colors.blue, width: 50, height:50 ),
+              Container( color: Colors.red, width: 50, height:50 ),
+              Container( color: Colors.green, width: 50, height:50 ),
+              Container( color: Colors.orange, width: 50, height:50 ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
