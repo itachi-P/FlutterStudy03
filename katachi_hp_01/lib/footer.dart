2eb4bda0fb45f7fabad17c8f543449b115b2ac56
itachi-P <itachip38@gmail.com>
2020-02-14 11:53:55 +0900

    Add header menu (implemented by Drawer) and footer menu (implemented by BottomNavigationBar).

--- katachi_hp_01/lib/footer.dart
+++ katachi_hp_01/lib/footer.dart
@@ -9,68 +9,11 @@ class Footer extends StatefulWidget {
 
 class _Footer extends State {
   int _selectedIndex = 0;
-  final _bottomNavigationBarItems = [];
-
-  // Icon's List
-  static const _footerIcons = [
-    Icons.home,
-    Icons.mail_outline,
-    Icons.person,
-  ];
-
-  //Icon's String
-  static const _footerItemNames = [
-    'HOME',
-    'CONTACT',
-    'LOGIN',
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-    _bottomNavigationBarItems.add(_UpdateActiveState(0));
-    for (var i = 1; i < _footerItemNames.length; i++) {
-      _bottomNavigationBarItems.add(_UpdateDeactiveState(i));
-    }
-  }
-
-  /// Activate index item
-  BottomNavigationBarItem _UpdateActiveState(int index) {
-    return BottomNavigationBarItem(
-      icon: Icon(
-        _footerIcons[index],
-        color: Colors.blueAccent,
-      ),
-      title: Text(
-        _footerItemNames[index],
-        style: TextStyle(
-          color: Colors.white38,
-        ),
-      ),
-    );
-  }
-
-  /// Deactivate index item
-  BottomNavigationBarItem _UpdateDeactiveState(int index) {
-    return BottomNavigationBarItem(
-      icon: Icon(
-        _footerIcons[index],
-        color: Colors.black12,
-      ),
-      title: Text(
-        _footerItemNames[index],
-        style: TextStyle(
-          color: Colors.black26,
-        ),
-      ),
-    );
-  }
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 10, fontWeight: FontWeight.bold);
 
   void _onItemTapped(int index) {
     setState(() {
-      _bottomNavigationBarItems[_selectedIndex] =
-          _UpdateDeactiveState(_selectedIndex);
-      _bottomNavigationBarItems[index] = _UpdateActiveState(index);
       _selectedIndex = index;
     });
   }
@@ -78,10 +21,28 @@ class _Footer extends State {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed, // Required when displaying 4 or more
-      items: _bottomNavigationBarItems,
-      currentIndex: _selectedIndex,
-      onTap: _onItemTapped,
-    );
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('HOME', style: optionStyle),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail_outline),
+            title: Text(
+              'CONTACT',
+              style: optionStyle,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text(
+              'LOGIN',
+              style: optionStyle,
+            ),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped);
   }
 }
--- katachi_hp_01/lib/main.dart
+++ katachi_hp_01/lib/main.dart
@@ -1,82 +1,173 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:katachi_hp_01/footer.dart';
-import 'package:katachi_hp_01/header.dart';
 
 void main() {
   debugPaintSizeEnabled = true;
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _State();
+  }
+}
+
+class _State extends State<MyApp> {
+  var _selected = '';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Katachi HP clone 02',
-      home: Scaffold(
-        appBar: Header(),
-        body: Center(
-          child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'images/katachi_logo01.png',
+          height: 30.0,
+          width: 200.0,
+          fit: BoxFit.contain,
+        ),
+        flexibleSpace: Container(
+          decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+                colors: [
+                  const Color(0xFF3366FF),
+                  const Color(0x0000CCFF),
+                ],
+                begin: const FractionalOffset(1.0, 0.0),
+                end: const FractionalOffset(0.0, 0.0),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.clamp),
+          ),
+        ),
+        actions: <Widget>[],
+      ),
+      drawer: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.blue, //This will change the drawer background
+        ),
+        child: Drawer(
+          child: ListView(
             children: <Widget>[
-              //Header(), // 外部ファイル化したヘッダーWidget（のインスタンス）を指定
-              Container(
-                width: 400,
-                //height: 150.0,
-                padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
-                child: Image.network(
-                    'http://goope.akamaized.net/66978/191108172416e7zr_l.jpg'),
-              ),
-              Container(
-                color: Colors.blue,
-                width: 400.0,
-                height: 150.0,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(15.0),
-                margin: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      '-就労移行支援事業所「未来のはまち」-\n経験ゼロのオマエをIT就労できる人材に躾けます\nオマエに必要なのは『隷属』だけ！！',
-                      style: TextStyle(
-                        fontSize: 13.0,
-                        fontWeight: FontWeight.bold,
-                        height: 1.7,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
+              ListTile(
+                title: Text(
+                  'HOME',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
                 ),
+                onTap: () {
+                  setState(() => _selected = 'HOME');
+                  Navigator.pop(context);
+                },
               ),
-              Container(
-                color: Colors.white,
-                width: 400.0,
-                height: 250.0,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Container(
-                      child: Image.asset(
-                        'images/katachi_row01.png',
-                        width: 160.0,
-                      ),
-                    ),
-                    Container(
-                      child: Image.asset(
-                        'images/katachi_row02.png',
-                        width: 160.0,
-                      ),
-                    ),
-                  ],
+              ListTile(
+                title: Text(
+                  'CONTACT',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
                 ),
+                onTap: () {
+                  setState(() => _selected = 'CONTACT');
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text(
+                  'LOGIN',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                onTap: () {
+                  setState(() => _selected = 'LOGIN');
+                  Navigator.pop(context);
+                },
               ),
             ],
           ),
         ),
-        bottomNavigationBar: Footer(),
       ),
+
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 400,
+              height: 140.0,
+              padding: EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+              child: Image.network(
+                  'http://goope.akamaized.net/66978/191108172416e7zr_l.jpg'),
+            ),
+            Container(
+              color: Colors.blue,
+              width: 400.0,
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(15.0),
+              margin: const EdgeInsets.fromLTRB(0, 0, 8.0, 8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    '-就労移行支援事業所「未来のはまち」-\n経験ゼロのオマエをIT就労できる人材に躾けます\nオマエに必要なのは『隷属』だけ！！',
+                    style: TextStyle(
+                      fontSize: 13.0,
+                      fontWeight: FontWeight.bold,
+                      height: 1.7,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              width: 400.0,
+              height: 250.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Container(
+                    child: Image.asset(
+                      'images/katachi_row01.png',
+                      width: 160.0,
+                    ),
+                  ),
+                  Container(
+                    child: Image.asset(
+                      'images/katachi_row02.png',
+                      width: 160.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Text(
+                _selected,
+                style: TextStyle(
+                  fontSize: 32,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Footer(), // TODO
     );
   }
 }
