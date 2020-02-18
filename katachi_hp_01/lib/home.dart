3ea03fd8f49a9e8753a42ca01641c2b15793d759
itachi-P <itachip38@gmail.com>
2020-02-18 14:11:40 +0900

    Tried using SizedBox and Stack on in a new home (temporary).

--- katachi_hp_01/lib/home.dart
+++ katachi_hp_01/lib/home.dart
@@ -15,30 +15,62 @@ class Home extends StatelessWidget {
             ),
             onPressed: () {
               // TODO
-              print("This menu is dummy.");
+              print("This menu is dummyだこりゃ。");
             },
           ),
         ],
       ),
-      body: Container(
-        width: 300.0,
-        height: 500.0,
-        padding: EdgeInsets.only(left: 30.0, top: 20.0),
-        margin: EdgeInsets.only(top: 80.0, left: 5.0),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.red,
-            width: 8.0,
-          ),
-        ),
-        child: Text(
-          '今現在実質的なホーム画面である'
-          '初期画面からHeaderとFooterだけを共通部分として残し、'
-          'メインのbody部分はメニュー選択によって入れ替わる静的画面の1つとして'
-          'このページ(home.dart/Homeクラス)に分離したいところ',
-          style: TextStyle(
-            fontSize: 16,
-          ),
+      body: SizedBox(
+        width: 300,
+        height: 400,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              width: 300,
+              height: 400,
+              color: Colors.purpleAccent,
+            ),
+            Container(
+              width: 290.0,
+              height: 250.0,
+              padding: EdgeInsets.only(left: 30.0, top: 20.0),
+              margin: EdgeInsets.only(top: 80.0, left: 5.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.red,
+                  width: 8.0,
+                ),
+              ),
+              child: Text(
+                '今現在実質的なホーム画面である'
+                '初期画面からHeaderとFooterだけを共通部分として残し、'
+                'メインのbody部分はメニュー選択によって入れ替わる静的画面の1つとして'
+                'このページ(home.dart/Homeクラス)に分離したいところ',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(5.0),
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: <Color>[
+                    Colors.black.withAlpha(0),
+                    Colors.black12,
+                    Colors.pink
+                  ],
+                ),
+              ),
+              child: Text(
+                "This is Foreground Textだす。",
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+          ],
         ),
       ),
     );
